f1d54d6e8d80efedf730891c7f34670367b0e5b7
Manuel Diaz <manuel.ade@gmail.com>
2018-09-19 21:34:27 +0800

    Eureka!

--- MUSLC_TVD_Genuine2D/HLLE2dFlux_Prototype.m
+++ MUSLC_TVD_Genuine2D/HLLE2dFlux_Prototype.m
@@ -5,7 +5,7 @@ global gamma; gamma = 1.4;
 
 % Build a 2x2 mesh
 dx=0.5; dy=0.5; [x,y]=meshgrid([0,1],[0,1]);
-[r0,u0,v0,p0] = Euler_IC2d(x,y,'Sod_x');
+[r0,u0,v0,p0] = Euler_IC2d(x,y,'Sod_y');
 E0 = p0./(gamma-1)+0.5*r0.*(u0.^2+v0.^2); % Total Energy
 c0 = sqrt(gamma*p0./r0);                  % Speed of sound
 q0 = cat(3, r0, r0.*u0, r0.*v0, E0);      % initial state
@@ -16,13 +16,6 @@ qSW = squeeze( q0( i , j ,:) );
 qSE = squeeze( q0( i ,j+1,:) );
 qNW = squeeze( q0(i+1, j ,:) );
 qNE = squeeze( q0(i+1,j+1,:) );
-
-
-% Just for comparison
-qL=q0(i  ,j,:); qR=q0( i ,j+1,:); fS=HLLE1Dflux(squeeze(qL),squeeze(qR),[1,0]);
-qL=q0(i+1,j,:); qR=q0(i+1,j+1,:); fN=HLLE1Dflux(squeeze(qL),squeeze(qR),[1,0]);
-qL=q0(i  ,j,:); qR=q0(i+1, j ,:); gW=HLLE1Dflux(squeeze(qL),squeeze(qR),[0,1]);
-qL=q0(i,j+1,:); qR=q0(i+1,j+1,:); gE=HLLE1Dflux(squeeze(qL),squeeze(qR),[0,1]);
 
 
 % West state
@@ -92,20 +85,20 @@ aEroe = sqrt( (gamma-1)*(HEroe-0.5*(uEroe^2+vEroe^2)) );
 
 
 % Wave speed estimates in the S
-sSW = min([ uSW-aSW, uSW+aSW, uSroe-aSroe, uSroe+aSroe ]);
-sSE = max([ uSE-aSE, uSE+aSE, uSroe-aSroe, uSroe+aSroe ]);
+sSW = min([ uSW-aSW, uSW+aSW, uSroe-aSroe, uSroe+aSroe ]); sSWplus=max(sSW,0);
+sSE = max([ uSE-aSE, uSE+aSE, uSroe-aSroe, uSroe+aSroe ]); sSEplus=max(sSE,0);
 
 % Wave speed estimates in the N
-sNW = min([ uNW-aNW, uNW+aNW, uNroe-aNroe, uNroe+aNroe ]);
-sNE = max([ uNE-aNE, uNE+aNE, uNroe-aNroe, uNroe+aNroe ]);
+sNW = min([ uNW-aNW, uNW+aNW, uNroe-aNroe, uNroe+aNroe ]); sNWplus=max(sNW,0);
+sNE = max([ uNE-aNE, uNE+aNE, uNroe-aNroe, uNroe+aNroe ]); sNEplus=max(sNE,0);
 
 % Wave speed estimates in the W
-sWS = min([ vSW-aSW, vSW+aSW, vWroe-aWroe, vWroe+aWroe ]);
-sWN = max([ vNW-aNW, vNW+aNW, vWroe-aWroe, vWroe+aWroe ]);
+sWS = min([ vSW-aSW, vSW+aSW, vWroe-aWroe, vWroe+aWroe ]); sWSplus=max(sWS,0);
+sWN = max([ vNW-aNW, vNW+aNW, vWroe-aWroe, vWroe+aWroe ]); sWNplus=max(sWN,0);
 
 % Wave speed estimates in the E
-sES = min([ vSE-aSE, vSE+aSE, vEroe-aEroe, vEroe+aEroe ]);
-sEN = max([ vNE-aNE, vNE+aNE, vEroe-aEroe, vEroe+aEroe ]);
+sES = min([ vSE-aSE, vSE+aSE, vEroe-aEroe, vEroe+aEroe ]); sESplus=max(sES,0);
+sEN = max([ vNE-aNE, vNE+aNE, vEroe-aEroe, vEroe+aEroe ]); sENplus=max(sEN,0);
 
 % The maximum wave speed delimit the interacting region to a square domain
 sS  = min(sWS,sES); 
@@ -149,16 +142,16 @@ gNW = [rNW*vNW; rNW*vNW*uNW; rNW*vNW*vNW + pNW; rNW*vNW*HNW];
 gNE = [rNE*vNE; rNE*vNE*uNE; rNE*vNE*vNE + pNE; rNE*vNE*HNE];
 
 % Compute the intermediate states
-qSO = ( sSE*qSE - sSW*qSW + fSW-fSE )/(sSE-sSW);
-qNO = ( sNE*qNE - sNW*qNW + fNW-fNE )/(sNE-sNW);
-qOW = ( sWN*qNW - sWS*qSW + gSW-gNW )/(sWN-sWS);
-qOE = ( sEN*qNE - sES*qSE + gSE-gNE )/(sEN-sES);
+qSO = ( sSE*qSE - sSW*qSW + fSW-fSE )/(sSE-sSW); % verified
+qNO = ( sNE*qNE - sNW*qNW + fNW-fNE )/(sNE-sNW); % verified
+qOW = ( sWN*qNW - sWS*qSW + gSW-gNW )/(sWN-sWS); % verified
+qOE = ( sEN*qNE - sES*qSE + gSE-gNE )/(sEN-sES); % verified
 
 % Compute the intermediate states fluxes (normal HLLE 1d fluxes)
-fSO = ( sSE*fSW - sSW*fSE + sSW*sSE*(qSE-qSW) )/(sSE-sSW);
-fNO = ( sNE*fNW - sNW*fNE + sNW*sNE*(qNE-qNW) )/(sNE-sNW);
-gOW = ( sWN*gSW - sWS*gNW + sWS*sWN*(qNW-qSW) )/(sWN-sWS);
-gOE = ( sEN*gSE - sES*gNE + sES*sEN*(qNE-qSE) )/(sEN-sES);
+fSO = ( sSE*fSW - sSW*fSE + sSW*sSE*(qSE-qSW) )/(sSE-sSW); % verified
+fNO = ( sNE*fNW - sNW*fNE + sNW*sNE*(qNE-qNW) )/(sNE-sNW); % verified
+gOW = ( sWN*gSW - sWS*gNW + sWS*sWN*(qNW-qSW) )/(sWN-sWS); % verified
+gOE = ( sEN*gSE - sES*gNE + sES*sEN*(qNE-qSE) )/(sEN-sES); % verified
 
 % Compute the transverse intermediate fluxes (Balsara's solution)
 fOW = [qOW(2);gOW(3)+(qOW(2)^2-qOW(3)^2)/qOW(1);qOW(3)*qOW(2)/qOW(1);qOW(2)*gOW(4)/(qOW(3)+eps)];
@@ -166,18 +159,23 @@ fOE = [qOE(2);gOE(3)+(qOE(2)^2-qOE(3)^2)/qOE(1);qOE(3)*qOE(2)/qOE(1);qOE(2)*gOE(
 gSO = [qSO(3);qSO(2)*qSO(3)/qSO(1);fSO(2)+(qSO(3)^2-qSO(2)^2)/qSO(1);qSO(3)*fSO(4)/(qSO(2)+eps)];
 gNO = [qNO(3);qNO(2)*qNO(3)/qNO(1);fNO(2)+(qNO(3)^2-qNO(2)^2)/qNO(1);qNO(3)*fNO(4)/(qNO(2)+eps)];
 
+% Verify with constant state
+%disp([fNW,fNO,fNE;fOW,nan(4,1),fOE;fSW,fSO,fSE]) % everything is good ;)
+%disp([gNW,gNO,gNE;gOW,nan(4,1),gOE;gSW,gSO,gSE]) % everything is good ;)
 
 % Area of the main quadrilateral
 aOO = (dt^2/2)*((sNE-sSW)*(sWN-sES)+(sEN-sWS)*(sSE-sNW)); %disp(aOO);
-a22 = polyarea([sNE,sNW,sSW,sSE]*dt,[sEN,sWN,sWS,sES]*dt); %disp(a22);
-disp(aOO==a22) % verified!
+%a22 = polyarea([sNE,sNW,sSW,sSE]*dt,[sEN,sWN,sWS,sES]*dt); %disp(a22);
+%disp(aOO==a22) % verified!
 
 % Strongly Interacting state q**
-qOO = 1/((sNE-sSW)*(sWN-sES)+(sEN-sWS)*(sSE-sNW))*( ...
-     (sWN*sNE+sSE*sEN)*qNE - (sEN*sNW+sSW*sWN)*qNW + ...
-     (sES*sSW+sNW*sWN)*qSW - (sWS*sSE+sNE*sES)*qSE ...
-   - sWN*fNE+sEN*fNW - sES*fSW+sWS*fSE - (sEN-sES)*fOE+(sWN-sWS)*fOW ...
-   - sSE*gNE+sSW*gNW - sNW*gSW+sNE*gSE - (sNE-sNW)*gNO+(sSE-sSW)*gSO );
+qOO = 1/((sNE-sSW)*(sWN-sES)+(sEN-sWS)*(sSE-sNW)) * ( ...
+  (sWN*sNE+sSE*sEN)*qNE - (sEN*sNW+sSW*sWN)*qNW + (sES*sSW+sNW*sWS)*qSW - (sWS*sSE+sNE*sES)*qSE ...
+   - sWN*fNE + sEN*fNW - sES*fSW + sWS*fSE - (sEN-sES)*fOE + (sWN-sWS)*fOW ...
+   - sSE*gNE + sSW*gNW - sNW*gSW + sNE*gSE - (sNE-sNW)*gNO + (sSE-sSW)*gSO );
+
+% Verify with constant state
+%disp([qNW,qNO,qNE;qOW,qOO,qOE;qSW,qSO,qSE]) % Everything is good ;D
 
 %% Form 0: Compute fluxes of the strongly interacting state
 % Precompute deltas
@@ -214,6 +212,10 @@ detM = (df1*dg2-df2*dg1)^2 + (df1*dg3-df3*dg1)^2 + (df2*dg4-df4*dg2)^2 + ...
 fOO = (k11*b1 + k12*b2 + k13*b3 + k14*b4)/detM;
 gOO = (k21*b1 + k22*b2 + k23*b3 + k24*b4)/detM;
 
+% Report q**, f** and g**
+disp('LSQ : q**, f**, g**');
+disp([qOO,fOO,gOO]);
+
 %% Form I
 A1=[sSE+sSW-sNE-sNW,sNW+sSW-sNE-sSE;...
     sES+sWS-sEN-sWN,sWN+sWS-sEN-sES];
@@ -224,6 +226,9 @@ for i=1:4
     f00(i)=F00(1); g00(i)=F00(2);
 end
 
+% Report q**, f** and g**
+disp('MthI : q**, f**, g**');
+disp([qOO,f00,g00]);
 
 %% Form II
 % Precompute c1 and c2
@@ -239,16 +244,50 @@ a21 = df4*dq2-df2*dq4;    a22 = dg4*dq2-dg2*dq4;
 
 % Explicit computation of the strongly Interacting state fluxes: f** and g**
 foo=( a22*c1-a12*c2)/(a11*a22-a12*a21);
-goo=(-a21*c1+a11*c2)/(a11*a22-a12*a21);
+goo=(-a21*c1+a11*c2)/(a11*a22-a12*a21); % verified!
 
-% Compare solutions of methods
-disp([fOO,f00,foo]);
-disp([gOO,g00,goo]);
+% Report f** and g**
+disp('MthII : q**, f**, g**');
+disp([nan(4,1),foo,goo]);
 
-disp('fN-fS-gW-gE');
-disp([fN,fS,gW,gE]);
+%% Balsara's HLLE 2-d
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1-d HLLE solutions
+i=1; j=1;
+qL=q0(i  ,j,:); qR=q0( i ,j+1,:); fS=HLLE1Dflux(squeeze(qL),squeeze(qR),[1,0]);
+qL=q0(i+1,j,:); qR=q0(i+1,j+1,:); fN=HLLE1Dflux(squeeze(qL),squeeze(qR),[1,0]);
+qL=q0(i  ,j,:); qR=q0(i+1, j ,:); gW=HLLE1Dflux(squeeze(qL),squeeze(qR),[0,1]);
+qL=q0(i,j+1,:); qR=q0(i+1,j+1,:); gE=HLLE1Dflux(squeeze(qL),squeeze(qR),[0,1]);
+
+% Display HLLE approximations for verification
+%disp('1-d HLLE individual solutions: fN - fS - gW - gE');
+%disp([fN,fS,gW,gE]); % verified!
+
+% Vector of coeficients
+d1= sS*sW*qSW-sS*sE*qSE-sN*sW*qNW+sN*sE*qNE...
+    + sS*(fSE-fSW)-sN*(fNE-fNW)+sE*(gSE-gNE)-sW*(gSW-gNW)...
+    +0.5*(sWS*(fSW-fOW)-sES*(fSE-fOE)-sWN*(fNW-fOW)+sEN*(fNE-fOE))...
+    +0.5*(sSW*(gSW-gSO)-sNE*(gSE-gSO)-sNW*(gNW-gNO)+sNE*(gNE-gNO));
+d2= -sS*sE*qSE + sN*sE*qNE + 0.5*(sSWplus*gSW-sNWplus*gNW+sN*fN-sS*fS)...
+    +0.5*((2*sE-sSEplus)*gSE-(2*sE-sNEplus)*gNE-(sNEplus-sNWplus)*gNO+(sSEplus-sSWplus)*gSO)...
+    -0.5*((-2*sS+sES)*fSE+(2*sN-sEN)*fNE+(sEN-sES)*fOE);
+d3= -sN*sW*qNW + sN*sE*qNE + 0.5*(sWSplus*fSW-sESplus*fSE+sE*gE-sW*gW)...
+    +0.5*((2*sN-sWNplus)*fNW-(2*sN-sENplus)*fNE-(sENplus-sESplus)*fOE+(sWNplus-sWSplus)*fOW)...
+    -0.5*((-2*sW+sNW)*gNW+(2*sE-sNE)*gNE+(sNE-sNW)*gNO);
+d = [d1,d2,d3]';
+
+% Define square region R'
+Rprime = (sE-sW)*(sN-sS)*dt^2;
+C = [(sE-sW)*(sN-sS),0,0; sE*(sN-sS),-0.5*(sN-sS),0; sN*(sE-sW),0,-0.5*(sE-sW)];
+Cinv = 1/((sE-sW)*(sN-sS))*[1,0,0;2*sE,-2*(sE-sW),0;2*sN,0,-2*(sN-sS)]; % verified
+
+% Solve 
+X = Cinv*d;
+disp('Balsara HLLE-2d : q**, f**, g**');
+disp(X');
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % The HLLE-1d (for testing)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
